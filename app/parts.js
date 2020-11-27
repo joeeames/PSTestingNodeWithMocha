7ad01905feaef574bf82f41085674e68e94a5450
Joe Eames <joeeames@gmail.com>
2020-11-27 15:37:32 -0700

    added more parts

--- app/parts.js
+++ app/parts.js
@@ -2,9 +2,15 @@ const Part = require('./models/Part.js');
 
 module.exports = [
     new Part("101","Flux Capacitor", 45, "Allows time travel. Also allows you to go back to the future.", 2),
-    new Part("102","Warp Core", 25, "Stuck in a rut? A brand new Warp Core will get you cruising along. Certified up to Warp 9.", 1500),
-    new Part("103","Hyperdrive", 25, "Need to get somehwere in a hurry? Nothing beats speed like lightspeed", 50),
-    new Part("104","T-14 Hyperdrive Generator", 25, "Nobody else has these but us!", 50),
+    new Part("102","Warp Core", 140, "Stuck in a rut? A brand new Warp Core will get you cruising along. Certified up to Warp 9.", 1500),
+    new Part("103","Hyperdrive", 60, "Need to get somehwere in a hurry? Nothing beats speed like lightspeed", 50),
+    new Part("104","T-14 Hyperdrive Generator", 25, "Nobody else has these but us!", 150),
     new Part("105","Phaser Coil", 65, "Pesky Ferenghi got you down? Nothing gets rid of unwanted pests like a top notch Phaser Coil.", 12),
-    new Part("","", 65, "", 12),
+    new Part("106","Beryllium Sphere", 99, "Get your warp drive back online quick!", 80),
+    new Part("107","Carbonite Fast Freezer", 55, "Keep your bounties safe in carbonite!", 12),
+    new Part("108","Hyperdrive Compressor", 15, "The quickest way to get your Hyperdrive back in shape", 3),
+    new Part("109","Holodeck", 135, "Keep your crew happy, with a relatively low chance of creating a SuperVillain", 900),
+    new Part("110","Photon Torpedos", 12, "Guaranteed to destroy your enemies! Totally different than Proton Torpedos", 8),
+    new Part("111","Proton Torpodes", 12, "Guaranteed to destroy your enemies! Totally different than Photon Torpedos", 8),
+    new Part("112","Escape Pod", 55, "Keep your crew alive in case of a complete ship loss. Life readings unreliable. No droids allowed.", 12),
 ]
